0d10672a7aba443e4af4c330551843e258543243
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-03-22 21:15:08 +1100

    Added rudimentary timer function. added logic to check if the answer chosen by the user is correct. Added functionality to move the user to the next question

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,11 @@
-
 var questions = [
-    "Inside which HTML element do we put Javascript?", 
+    "Which HTML element do we put Javascript inside?", 
     "what is the correct attribute to reference an external script?",
     "How do you define a function",
     "What is the correct way to add a comment to Javascript",
     "How do you write an array?",
     "what is the keyword to declare a variable",
-    "which of the following is NOT a type of variable",
+    "which of the following is NOT a data type in Javascript?",
     "Which of these is NOT a mathematical operator in Javascript",
     "Which of these statments is NOT valid javascript",
     "which of these is the best practice for naming variables in Javascript"
@@ -18,9 +17,9 @@ var answers = [
     ["# Comment", "<!--Comment-->", "//Comment", "*Comment"],
     ["[a,b,c]", "{a,b,c}", "(a,b,c)", "<a,b,c>"],
     ["var", "v", "declare", "variable"],
-    ["Boolean, String, Number, Long"],
+    ["Boolean", "String", "Number", "Long"],
     ["+", "x", "%", "/"],
-    ["x = a + b", "var x, a, b", "y =!= z", "'String' + 'other string'"],
+    ["x = a + b;", "var x, a, b;", "y =!= z;", "'String' + 'other string';"],
     ["variable-name", "variable_name", "VariableName", "variableName"]
 ];
 var correctAnswers = [
@@ -36,35 +35,61 @@ var correctAnswers = [
     "variableName"
 ];
 
-if (questions.length !== answers.length || answers.length !== correctAnswers.length || questions.length !== correctAnswers.length) {
-    alert("alignment error");
-}
-
 var startButton = document.getElementById("start-button");
 var questionBox = document.getElementById("question-box");
 var questionSentence = document.getElementById("question-sentence");
 var answerBox = document.getElementById("answer-box");
 var answerSpaces = answerBox.getElementsByTagName('li');
 
+var questionNumber = -1;
+var score = 0;
+var ticker = 60;
 
-function newQuestion(num) {
-    questionSentence.textContent = questions[num];
-
+function newQuestion() {
+    if (questionNumber < 9) {
+        questionNumber++;
+    } else {
+        return;
+    }
+    questionSentence.textContent = questions[questionNumber];
     for (i = 0; i < answerSpaces.length; i++) {
-        answerSpaces[i].textContent = answers[num][i];
+        answerSpaces[i].setAttribute('data-answer', answers[questionNumber][i])
+        answerSpaces[i].textContent = answers[questionNumber][i];
     }
 }
-
+function onTimer() {
+    if (ticker < 10) {
+        document.getElementById('timer').innerHTML = "timer: 0" +ticker;
+    } else {
+        document.getElementById('timer').innerHTML = "timer: " +ticker;
+    }
+    ticker--;
+    if (ticker < 0) {
+        alert('You lose!');
+    }
+    else {
+        setTimeout(onTimer, 1000);
+    }
+}
 
 startButton.addEventListener("click", function() {
     questionBox.style.display = "block";
     startButton.style.display = "none";
-    newQuestion(0);
+    newQuestion();
+    onTimer();
 
 });
 
 for (var i = 0; i < answerSpaces.length; i++) {
     answerSpaces[i].addEventListener("click", function(e) {
-        console.log(e.target);
+        console.log();
+        if (e.target.getAttribute("data-answer") === correctAnswers[questionNumber]) {
+            console.log("Correct");
+            score++
+        } else {
+            console.log("Incorrect");
+            //decrement timer
+        }
+        newQuestion();
     });
 }
